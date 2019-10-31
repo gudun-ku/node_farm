d726f6f70ade19a584c0d30ba6c3ef4679c72ecb
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-10-31 11:47:39 +0300

    Adds products page. Removes template filling into a module

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
+const replaceTemplate = require("./modules/replacetemplate");
 
 //////////////////////////////////////////////////////////////////////////
 // FILES
@@ -30,24 +32,50 @@ const http = require("http");
 /////////////////////////////////////////////////////////////////////////
 // SERVER
 
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);           
 
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
 
-    
-    if (pathName === "/" | pathName === "/overview") {
-        res.end("This is the OVERVIEW");        
-    } else if (pathName === '/product') {
-        res.end("This is the PRODUCT");    
-    } else if (pathName === '/api') {      
+    const { query, pathname } = url.parse(req.url, true);
+    //console.log(pathname);
+
+    // Overview page
+    if (pathname === "/" | pathname === "/overview") {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace(/{%PRODUCT_CARDS%}/, cardsHtml);
+        //console.log(cardsHtml);
+        res.end(output);        
+
+    // Product page   
+    } else if (pathname === "/product") {
+        //console.log(query);
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+        res.end(output);    
+
+    // Api    
+    } else if (pathname === "/api") {      
         res.writeHead(200,{'Content-type': 'application/json'});  
         res.end(data);      
+
+    // Not found    
     } else {
         res.writeHead(404, {
-            'Content-type': 'text/thml',
+            'Content-type': 'text/html',
             'my-own-header': 'hello-world'
         });
         res.end('<h1>Page not found!</h1>');
