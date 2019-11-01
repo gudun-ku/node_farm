a0696a9d5f320eb6fabf257561890fce06767537
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-11-01 18:11:19 +0300

    Tests slugify package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replacetemplate");
 
 //////////////////////////////////////////////////////////////////////////
@@ -37,7 +38,10 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObj = JSON.parse(data);           
+const dataObj = JSON.parse(data);   
+
+const slugs = dataObj.map(el => slugify(el.productName, {lower: true}));
+console.log(slugs);
 
 
 const server = http.createServer((req, res) => {
